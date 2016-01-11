542544fa1226edda9fca1704d9aeaba5bd8cae27
cdev38399 <cdev38399@gmail.com>
2016-01-12 02:55:07 +0700

    Create AutoloadExample.php

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace cdev38399\d3wordcloud;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
