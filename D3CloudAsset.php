a098cffda885e7f0adc9731d5ccdff32e3441daf
cdev38399 <cdev38399@gmail.com>
2016-01-12 02:56:36 +0700

    Create D3CloudAsset.php

---
+++ D3CloudAsset.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * D3 Cloud Asset
+ * @author cdev38399 <cdev38399@gmail.com>
+ */
+
+namespace cdev38399\d3wordcloud;
+
+use yii\web\AssetBundle;
+
+class D3CloudAsset extends AssetBundle {
+
+    public $sourcePath = '@bower/d3-cloud';
+    public $css = [];
+    public $js = ['index.js'];
+
+}
